cf3129d807a7741545b40d43f7accae45d309f3b
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-03-31 21:29:31 -0300

    main

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,34 @@
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
+// import Sequelize from 'sequelize'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
-export const connection = new Sequelize(
-    process.env.DATABASE_URL, 
+// export const connection = new Sequelize(
+//     process.env.DATABASE_URL, 
+//     {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+//     }
+// )
+
+import Sequelize from 'sequelize';//conecta com banco de dados
+import dotenv from 'dotenv'//nao subir dados sensiveis
+
+dotenv.config()//variavel interna de ambiente pra configurar o servidor
+
+export const connection = new Sequelize( //faz a conexão
+    process.env.DB_BASE,//acessando todos os config apartir do .env
+    process.env.DB_USER,//tem que ser nesta ordem
+    process.env.DB_PASS,
+
     {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+            host: process.env.DB_HOST,
+            port: 5432,
+            dialect:'postgres'
     }
 )
